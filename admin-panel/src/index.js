cd3bae8d648f291cfffb818a76fc9c1faaaf7fd6
sintayehu-dev <sintayehubelfa23@gmail.com>
2025-07-25 05:00:07 -0700

    Remove unused reportWebVitals import from index.js to improve code clarity.

--- admin-panel/src/index.js
+++ admin-panel/src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 // Import your custom CSS
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals'; // Removed unused import
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,7 +16,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Removed reportWebVitals();
